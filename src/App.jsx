e7804f69c85fedf6767cbf0a33cdc8edd61301dd
Huitson <124617896+huitson-pereira@users.noreply.github.com>
2023-03-08 16:31:31 -0300

    teste de commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,27 +6,16 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
+    <div>
+      <h1>Huitson Orlins Pereira</h1>
+      <h2>idade: 58 anos</h2>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <img src="" alt="" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>
+        <a href="http://"></a>
+        <a href="http://"></a>
+      </div>    
     </div>
   )
 }
