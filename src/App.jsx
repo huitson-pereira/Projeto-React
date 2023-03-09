343c52b60a763db93761ee5b60e96edf3878d941
Huitson <124617896+huitson-pereira@users.noreply.github.com>
2023-03-09 18:16:28 -0300

    Commit final

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,12 @@ import CardApresentacao from "./components/CardApresentacao.jsx"
 function App() {
   
   return (
-    <CardApresentacao foto="https://avatars.githubusercontent.com/u/124617896?s=400&u=622eeb0f38233e6a2ad05a026745ce4d13da631f&v=4" nome ="Huitson Orlins Pereira" idade = "58" git="https://github.com/huitson-pereira/Turma-Itaguacu" linkedin="https://www.linkedin.com/in/huitson-pereira-6a7393163/" />
+    <CardApresentacao 
+    foto="https://avatars.githubusercontent.com/u/124617896?s=400&u=622eeb0f38233e6a2ad05a026745ce4d13da631f&v=4" 
+    nome ="Huitson Orlins Pereira" 
+    idade = "58" 
+    git="https://github.com/huitson-pereira" 
+    linkedin="https://www.linkedin.com/in/huitson-pereira-6a7393163/" />
   )
 }
 
--- src/components/CardApresentacao.jsx
+++ src/components/CardApresentacao.jsx
@@ -10,8 +10,16 @@ function CardApresentacao(props) {
             <h2 id="idade">idade: {props.idade} anos</h2>
           </div>
           <div id="social">
-            <a id="git" href={props.git} target ="_blank"> <box-icon type='logo' name="github" color='white' size='md' title='Link para o github'></box-icon> </a>
-            <a id="linkedin" href={props.linkedin} target ="_blank"> <box-icon type='logo' name="Linkedin" color='white' size='md' title='Link para o Linkedin'></box-icon></a>
+            <div id="git">
+              <a href={props.git} target ="_blank"> 
+                <box-icon type='logo' name="github" color='white' size='md' title='Link para o github'></box-icon> 
+              </a>
+            </div>
+            <div id="linkedin">
+              <a href={props.linkedin} target ="_blank"> 
+                <box-icon type='logo' name='linkedin-square' color='white' size='md' title='Link para o Linkedin'></box-icon>
+              </a>
+            </div>
           </div>
         </div>    
       </div>
